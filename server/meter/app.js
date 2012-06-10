e5a755b0dd1e4d33f9892f15105aef03229805a8
Hans <martin@ubuntu.(none)>
2012-06-11 01:58:06 +0200

    Server is now an express app, which serves both the static HTML and the real time content.

--- server/meter/app.js
+++ server/meter/app.js
@@ -1,6 +1,44 @@
-var io = require('socket.io').listen(8000); // server listens for socket.io communication at port 8000
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+io = require('socket.io').listen(app);
 io.set('log level', 1); // disables debugging. this is optional. you may remove it if desired.
 
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+// talk to arduino
 
 var SerialPort  = require('serialport').SerialPort;
 var portName = '/dev/ttyUSB1';
@@ -21,6 +59,8 @@ sp.on('data', function (data) { // call back when data is received
     }
 });
 
+
+// talk to web clients
 io.sockets.on('connection', function (socket) {
     console.log('connected');
     socket.emit('counter', counterValue);
@@ -34,3 +74,5 @@ io.sockets.on('connection', function (socket) {
         console.log('disconnected');
     });
 });
+
+
